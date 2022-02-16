a6c54eca245ae0a646d105113c4dc81253f5121f
justfantasy <27506954@qq.com>
2022-02-16 08:51:39 +0800

    1380

--- leet-code/src/main.ts
+++ leet-code/src/main.ts
@@ -1,5 +1,6 @@
 import './style.css'
-import twoSum from './questions/0001.twoSum';
+// import twoSum from './solutions/0001.twoSum';
+import luckyNumbers from './solutions/1380.luckyNumbers';
 
-console.log(twoSum([1, 2, 3, 4], 6));
+console.log(luckyNumbers([[3,7,8],[9,11,13],[15,16,17]]));
 
---
+++ leet-code/src/solutions/1380.luckyNumbers.ts
@@ -0,0 +1,32 @@
+/**
+ * 给你一个 m * n 的矩阵，矩阵中的数字 各不相同 。请你按 任意 顺序返回矩阵中的所有幸运数。
+ *
+ * 幸运数是指矩阵中满足同时下列两个条件的元素：
+ *
+ * 在同一行的所有元素中最小
+ * 在同一列的所有元素中最大
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/lucky-numbers-in-a-matrix
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @param matrix
+ */
+export default function luckyNumbers(matrix: number[][]): number[] {
+  let luckyNumbers: number[] = [];
+  let length = matrix.length;
+  for (let i = 0; i < length; i++) {
+    // 获取当前行的最小值
+    let min = Math.min(...matrix[i]);
+
+    // 最小值所在的列
+    let minInx = matrix[i].findIndex((ele) => {
+      return ele === min;
+    });
+
+    // 是否是当前列中的最大值
+    if (!matrix.some((ele) => ele[minInx] > min)) {
+      luckyNumbers = [min, ...luckyNumbers];
+    }
+  }
+  return luckyNumbers;
+}
