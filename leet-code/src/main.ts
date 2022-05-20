6dd83b95bb5669750c6ece96d34b148c73b6f3c1
27506954@qq.com <27506954@qq.com>
2022-05-20 17:56:24 +0800

    1

--- leet-code/src/main.ts
+++ leet-code/src/main.ts
@@ -1,6 +1,19 @@
-import './style.css'
+import './style.css';
 // import twoSum from './solutions/0001.twoSum';
-import longestPalindrome from './solutions/0005.longestPalindrome';
+// import longestPalindrome from './solutions/0005.longestPalindrome';
 
-console.log(longestPalindrome('abcd'));
+interface Rabbit {
+  name: string;
+  age: number;
+}
 
+type ReadonlyRabbit<T> = {
+  readonly [K in keyof T]: T[K];
+}
+
+const rabbit: ReadonlyRabbit<Rabbit> = {
+  name: 'Liz',
+  age: 6,
+};
+
+rabbit.age = 7;
